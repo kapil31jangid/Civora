04e8047afaf0303d351761cc64f68ce3c905a050
kapil31jangid <kapil31jangid@gmail.com>
2026-09-11 20:21:04 +0530

    fix: set type module in root package.json for Vercel lambda ESM

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,16 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { createApp } from '../server/src/app.js'
 
-const app = createApp()
+let appInstance: ReturnType<typeof createApp> | undefined
+
+function getApp() {
+  if (!appInstance) {
+    appInstance = createApp()
+  }
+  return appInstance
+}
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  const app = getApp()
   return app(req, res)
 }
